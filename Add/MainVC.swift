30c034497f0f425aba6a428d000bbf75a9a6394d
dd <dd@dds-imac.local>
2015-11-15 22:11:17 +0100

    partial animated multi-choice prototype --> generate button not really working

--- Add/MainVC.swift
+++ Add/MainVC.swift
@@ -7,36 +7,99 @@
 //
 
 import UIKit
+import Foundation
 
 class MainVC: UIViewController {
 
     
-    @IBOutlet weak var refView: UIView!
+    var refView: UIView!
+    let gravity = UIGravityBehavior()
+    lazy var animator: UIDynamicAnimator = {
+        let lazyAnimator = UIDynamicAnimator(referenceView: self.refView)
+        return lazyAnimator
+    }()
+    
+    lazy var collider: UICollisionBehavior = {
+        let lazyCollider = UICollisionBehavior()
+        lazyCollider.translatesReferenceBoundsIntoBoundary = true
+        return lazyCollider
+    }()
 
     @IBOutlet weak var display: UILabel!
     
-    @IBOutlet weak var displayResult: UILabel!
- 
-    @IBOutlet weak var correctnessDisplay: UILabel!
+//    @IBOutlet weak var displayResult: UILabel!
+// 
+//    @IBOutlet weak var correctnessDisplay: UILabel!
     
     @IBOutlet weak var timerDisplay: UILabel!
     
+    var numChoices:Int = 4
     var timer = NSTimer()
     var startTime = NSTimeInterval()
     
     var expectedResult: Int32 = 0
     var numOfFailedTry = 0
     let operation = "+"
+    let locationResolution = 10
+    
+    var bottonSize:CGSize!
+    
     
     override func viewDidLoad() {
-        let frame = CGRect(x: refView.bounds.width/2, y: refView.bounds.height/2, width: refView.bounds.width*0.8, height: refView.bounds.height/2)
+        super.viewDidLoad()
+        
+        let frame = CGRect(x: CGPointZero.x, y: CGPointZero.y, width: self.view.bounds.width, height: self.view.bounds.height*0.8)
         let displayView = UIView(frame: frame)
-        displayView.backgroundColor = UIColor.blueColor()
-        displayView.alpha = 0.5
+//        displayView.backgroundColor = UIColor.blueColor()
+//        displayView.alpha = 0.5
+        
+        self.view.addSubview(displayView)
+        refView = displayView
+        
+        let size = refView.bounds.size.width/CGFloat(locationResolution)
+        bottonSize = CGSize(width: size, height: size)
+        animator.addBehavior(gravity)
+        animator.addBehavior(collider)
     }
     
     @IBAction func generateAddTask(sender: UITapGestureRecognizer) {
         generateCalcTaskCore()
+        dropMultiChoice(numChoices, correctResult: expectedResult)
+        
+    }
+    
+    func dropMultiChoice(numChoices:Int, correctResult: Int32){
+        var multiChoices = [Int32](count:numChoices, repeatedValue: 0)
+        var buttons = [UIButton]()
+        let idxCorrectAnswer = Int(getRandom0ToN(3))
+        for i in 0...(numChoices-1){
+            
+            if i==idxCorrectAnswer{
+                
+                multiChoices[idxCorrectAnswer] = correctResult
+            }else{
+                let randUp = max(2*correctResult,20)
+                multiChoices[i] = getRandom0ToN(randUp)
+            }
+            
+            buttons.append(getUIButton("\(multiChoices[i])", buttonSize: bottonSize))
+            refView.addSubview(buttons[i])
+            
+            gravity.addItem(buttons[i])
+            collider.addItem(buttons[i])
+//            sleep(1)
+        }
+        
+    }
+    
+
+    
+    func answerIsCorrect(){
+        print("the answer is correct")
+            }
+    
+    func answerIsWrong(){
+        print("the answer is WRONG")
     }
     
     func generateCalcTaskCore() {
@@ -63,8 +126,8 @@ class MainVC: UIViewController {
             break
         }
 
-        displayResult.text = ""
-        correctnessDisplay.text = ""
+//        displayResult.text = ""
+//        correctnessDisplay.text = ""
         
         // stop watch start
         let aSelector : Selector = "updateTime"
@@ -103,33 +166,33 @@ class MainVC: UIViewController {
     
     var isNegative = false
     
-    @IBAction func negative() {
-        isNegative = true
-        displayResult.text = "-"
-        displayToBeCleanedUpNextTime = false
-    }
-
-    @IBAction func appendDigit(sender: UIButton) {
-        
-        let intDigit = NSNumberFormatter().numberFromString(sender.currentTitle!)!.intValue
-        digitStack.append(intDigit)
-//        var dispStr = ""
-        if displayToBeCleanedUpNextTime {
-            displayResult.text = "\(intDigit)"
-            displayToBeCleanedUpNextTime = false
-        }
-        else{
-            displayResult.text = displayResult.text! + "\(intDigit)"
-        }
-        print("displayResult.text  = " + displayResult.text!)
-        
-//        if (isNegative && isFirstCharNotMinus(displayResult.text!)) {
-//            displayResult.text = "-" + displayResult.text!
-//        }else{
-//            displayResult.text = displayResult.text!
+//    @IBAction func negative() {
+//        isNegative = true
+//        displayResult.text = "-"
+//        displayToBeCleanedUpNextTime = false
+//    }
+//
+//    @IBAction func appendDigit(sender: UIButton) {
+//        
+//        let intDigit = NSNumberFormatter().numberFromString(sender.currentTitle!)!.intValue
+//        digitStack.append(intDigit)
+////        var dispStr = ""
+//        if displayToBeCleanedUpNextTime {
+//            displayResult.text = "\(intDigit)"
+//            displayToBeCleanedUpNextTime = false
 //        }
-
-    }
+//        else{
+//            displayResult.text = displayResult.text! + "\(intDigit)"
+//        }
+//        print("displayResult.text  = " + displayResult.text!)
+//        
+////        if (isNegative && isFirstCharNotMinus(displayResult.text!)) {
+////            displayResult.text = "-" + displayResult.text!
+////        }else{
+////            displayResult.text = displayResult.text!
+////        }
+//
+//    }
     
     private func isFirstCharNotMinus( s: String) -> Bool{
         let c = s[s.startIndex]
@@ -142,58 +205,61 @@ class MainVC: UIViewController {
     }
     
     
-    @IBAction func removeLastEntry(sender: AnyObject) {
-        if digitStack.count>0{
-            digitStack.removeLast()
-            (_, displayResult.text!) = stackToNumber(digitStack)
-        }
-    }
+//    @IBAction func removeLastEntry(sender: AnyObject) {
+//        if digitStack.count>0{
+//            digitStack.removeLast()
+//            (_, displayResult.text!) = stackToNumber(digitStack)
+//        }
+//    }
     
     private func getRandom0To100Operand()->Int32{
-        let seed = UInt32(NSDate().timeIntervalSince1970)
-//        srand(seed)
-        let r = arc4random_uniform(seed)
-        return Int32( Double(r)*100.0 / Double(Int32.max))
+        return getRandom0ToN(100)
     }
     
-    @IBAction func enterResult() {
-        var result: Int32
-        (result, _) = stackToNumber(digitStack)
-        
-        if isNegative {
-            result = -result
-            isNegative = false
-        }
-        
-        print("result = \(result)")
-        displayToBeCleanedUpNextTime = true
-        
-        if result == expectedResult {
-            correctnessDisplay.font.fontWithSize(30)
-            correctnessDisplay.text = "Richtig 😊 Du bist Super !! "
-            
-            // stop timer
-            timer.invalidate()
-//            //timer = nil
-            
-        }else{
-            correctnessDisplay.font.fontWithSize(25)
-            if numOfFailedTry<=1{
-                correctnessDisplay.text = "Nicht ganz (\(numOfFailedTry)) 😐, Versuch nochmal ? "
-            }else if numOfFailedTry==2{
-                correctnessDisplay.text = "Noch nicht richtig (\(numOfFailedTry)) 😮, noch mal ! "
-            }
-            else if numOfFailedTry==3{
-                correctnessDisplay.text = "Letzte Versuch (\(numOfFailedTry)) 😓 ! "
-            }else{
-                correctnessDisplay.text = "😞"
-                display.text = "Richtige Ergebnis: " + display.text! + "\(expectedResult)"
-            }
-            
-            numOfFailedTry++
-        }
-        digitStack.removeAll(keepCapacity: false)
+    private func getRandom0ToN(n:Int32)->Int32{
+        let seed = UInt32(NSDate().timeIntervalSince1970)
+        let r = arc4random_uniform(seed)
+        return Int32( Double(r) * Double(n) / Double(Int32.max))
     }
+    
+//    @IBAction func enterResult() {
+//        var result: Int32
+//        (result, _) = stackToNumber(digitStack)
+//        
+//        if isNegative {
+//            result = -result
+//            isNegative = false
+//        }
+//        
+//        print("result = \(result)")
+//        displayToBeCleanedUpNextTime = true
+//        
+//        if result == expectedResult {
+//            correctnessDisplay.font.fontWithSize(30)
+//            correctnessDisplay.text = "Richtig 😊 Du bist Super !! "
+//            
+//            // stop timer
+//            timer.invalidate()
+////            //timer = nil
+//            
+//        }else{
+//            correctnessDisplay.font.fontWithSize(25)
+//            if numOfFailedTry<=1{
+//                correctnessDisplay.text = "Nicht ganz (\(numOfFailedTry)) 😐, Versuch nochmal ? "
+//            }else if numOfFailedTry==2{
+//                correctnessDisplay.text = "Noch nicht richtig (\(numOfFailedTry)) 😮, noch mal ! "
+//            }
+//            else if numOfFailedTry==3{
+//                correctnessDisplay.text = "Letzte Versuch (\(numOfFailedTry)) 😓 ! "
+//            }else{
+//                correctnessDisplay.text = "😞"
+//                display.text = "Richtige Ergebnis: " + display.text! + "\(expectedResult)"
+//            }
+//            
+//            numOfFailedTry++
+//        }
+//        digitStack.removeAll(keepCapacity: false)
+//    }
     
     private func stackToNumber(stack:[Int32])-> (Int32, String){
         var stackReverse = Array(stack.reverse())
@@ -237,6 +303,75 @@ class MainVC: UIViewController {
         timerDisplay.text = "\(strMinutes):\(strSeconds):\(strFraction)"
 //        timerDisplay.text = "\(strMinutes):\(strSeconds)"
         
+    }
+    
+    func getUIButton(text:String, buttonSize:CGSize) -> UIButton{
+        let button = UIButton()
+        button.frame = CGRect(origin: CGPointZero, size: buttonSize)
+        //        button.frame.origin.x = CGFloat.random(widthRatio) * buttonSize.width
+        button.frame.origin.x = CGFloat.random(10) * buttonSize.width
+        button.layer.cornerRadius = 18
+        button.backgroundColor = UIColor.random
+        button.setTitle(text, forState: .Normal)
+        button.addTarget(self, action: "pressed", forControlEvents: .TouchUpInside)
+        
+        return button
+    }
+    
+    func getUIButton(text:String, buttonSize:CGSize, xLocation:CGFloat) -> UIButton{
+        let button = UIButton()
+        button.frame = CGRect(origin: CGPointZero, size: buttonSize)
+        //        button.frame.origin.x = CGFloat.random(widthRatio) * buttonSize.width
+        button.frame.origin.x = xLocation
+        button.layer.cornerRadius = 18
+        button.backgroundColor = UIColor.random
+        button.setTitle(text, forState: .Normal)
+        button.addTarget(self, action: Selector("pressed:"), forControlEvents: .TouchUpInside)
+        
+        return button
+    }
+    
+    func pressed(sender:UIButton!){
+        print("button pressed")
+        timer.invalidate()
+    }
+    
+    func chooseResult(sender: UIButton!){
+        let chosenAnswer = Int32(sender.currentTitle!)
+        
+        if chosenAnswer == expectedResult {
+            answerIsCorrect()
+        }else{
+            answerIsWrong()
+        }
+        // stop timer
+        timer.invalidate()
+        //            //timer = nil
+        
+        
+    }
+    
+    
+}
+
+private extension CGFloat{
+    static func random(max:Int)->CGFloat{
+        let x = arc4random() % UInt32(max)
+        return CGFloat(x)
+    }
+}
+
+private extension UIColor {
+    class var random:UIColor{
+        switch arc4random()%5{
+        case 0: return UIColor.greenColor()
+        case 1: return UIColor.blueColor()
+        case 2: return UIColor.orangeColor()
+        case 3: return UIColor.redColor()
+        case 4: return UIColor.purpleColor()
+        default: return UIColor.blackColor()
+            
+        }
     }
 }
 
---
+++ Add/Util.swift
@@ -0,0 +1,18 @@
+//
+//  Util.swift
+//  Add
+//
+//  Created by dd on 15/11/15.
+//  Copyright © 2015 dnhCollection. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class Util{
+    
+
+}
+
+
